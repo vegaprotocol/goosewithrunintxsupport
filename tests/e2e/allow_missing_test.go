pressly/goose

feat: support passing interface as connection instead of concrete type to goose api

tests/e2e/allow_missing_test.go
@@ -1,7 +1,7 @@
 package e2e
 
 import (
-	"database/sql"
+	"github.com/pressly/goose/v3/internal"
 	"testing"
 
 	"github.com/pressly/goose/v3"
@@ -317,8 +317,8 @@ func TestMigrateAllowMissingDown(t *testing.T) {
 
 // setupTestDB is helper to setup a DB and apply migrations
 // up to the specified version.
-func setupTestDB(t *testing.T, version int64) *sql.DB {
-	db, err := newDockerDB(t)
+func setupTestDB(t *testing.T, version int64) internal.GooseDB {
+	db, err := newDockerDB(t, true)
 	check.NoError(t, err)
 
 	// Create goose table.

tests/e2e/migrations_test.go
@@ -1,9 +1,10 @@
 package e2e
 
 import (
-	"database/sql"
+	"context"
 	"errors"
 	"fmt"
+	"github.com/pressly/goose/v3/internal"
 	"path/filepath"
 	"reflect"
 	"testing"
@@ -15,7 +16,7 @@ import (
 func TestMigrateUpWithReset(t *testing.T) {
 	t.Parallel()
 
-	db, err := newDockerDB(t)
+	db, err := newDockerDB(t, true)
 	check.NoError(t, err)
 	migrations, err := goose.CollectMigrations(migrationsDir, 0, goose.MaxVersion)
 	check.NoError(t, err)
@@ -43,7 +44,7 @@ func TestMigrateUpWithReset(t *testing.T) {
 func TestMigrateUpWithRedo(t *testing.T) {
 	t.Parallel()
 
-	db, err := newDockerDB(t)
+	db, err := newDockerDB(t, true)
 	check.NoError(t, err)
 	migrations, err := goose.CollectMigrations(migrationsDir, 0, goose.MaxVersion)
 	check.NoError(t, err)
@@ -80,7 +81,7 @@ func TestMigrateUpTo(t *testing.T) {
 	const (
 		upToVersion int64 = 2
 	)
-	db, err := newDockerDB(t)
+	db, err := newDockerDB(t, true)
 	check.NoError(t, err)
 	migrations, err := goose.CollectMigrations(migrationsDir, 0, goose.MaxVersion)
 	check.NoError(t, err)
@@ -101,7 +102,7 @@ func TestMigrateUpTo(t *testing.T) {
 func TestMigrateUpByOne(t *testing.T) {
 	t.Parallel()
 
-	db, err := newDockerDB(t)
+	db, err := newDockerDB(t, true)
 	check.NoError(t, err)
 	migrations, err := goose.CollectMigrations(migrationsDir, 0, goose.MaxVersion)
 	check.NoError(t, err)
@@ -131,7 +132,7 @@ func TestMigrateUpByOne(t *testing.T) {
 func TestMigrateFull(t *testing.T) {
 	t.Parallel()
 
-	db, err := newDockerDB(t)
+	db, err := newDockerDB(t, false)
 	check.NoError(t, err)
 	migrations, err := goose.CollectMigrations(migrationsDir, 0, goose.MaxVersion)
 	check.NoError(t, err)
@@ -208,9 +209,9 @@ func TestMigrateFull(t *testing.T) {
 	}
 }
 
-func getCurrentGooseVersion(db *sql.DB, gooseTable string) (int64, error) {
+func getCurrentGooseVersion(db internal.GooseDB, gooseTable string) (int64, error) {
 	var gotVersion int64
-	if err := db.QueryRow(
+	if err := db.QueryRowContext(context.Background(),
 		fmt.Sprintf("select max(version_id) from %s", gooseTable),
 	).Scan(&gotVersion); err != nil {
 		return 0, err
@@ -218,9 +219,9 @@ func getCurrentGooseVersion(db *sql.DB, gooseTable string) (int64, error) {
 	return gotVersion, nil
 }
 
-func getGooseVersionCount(db *sql.DB, gooseTable string) (int64, error) {
+func getGooseVersionCount(db internal.GooseDB, gooseTable string) (int64, error) {
 	var gotVersion int64
-	if err := db.QueryRow(
+	if err := db.QueryRowContext(context.Background(),
 		fmt.Sprintf("SELECT count(*) FROM %s WHERE version_id > 0", gooseTable),
 	).Scan(&gotVersion); err != nil {
 		return 0, err
@@ -228,7 +229,7 @@ func getGooseVersionCount(db *sql.DB, gooseTable string) (int64, error) {
 	return gotVersion, nil
 }
 
-func getTableNames(db *sql.DB) ([]string, error) {
+func getTableNames(db internal.GooseDB) ([]string, error) {
 	var query string
 	switch *dialect {
 	case dialectPostgres:
@@ -236,7 +237,7 @@ func getTableNames(db *sql.DB) ([]string, error) {
 	case dialectMySQL:
 		query = `SELECT table_name FROM INFORMATION_SCHEMA.tables WHERE TABLE_TYPE='BASE TABLE' ORDER BY table_name`
 	}
-	rows, err := db.Query(query)
+	rows, err := db.QueryContext(context.Background(), query)
 	if err != nil {
 		return nil, err
 	}

tests/gomigrations/error/testdata/002_ERROR_insert_no_tx.go
@@ -1,8 +1,8 @@
 package gomigrations
 
 import (
-	"database/sql"
 	"fmt"
+	"github.com/pressly/goose/v3/internal"
 
 	"github.com/pressly/goose/v3"
 )
@@ -11,7 +11,7 @@ func init() {
 	goose.AddMigrationNoTx(up002, nil)
 }
 
-func up002(db *sql.DB) error {
+func up002(db internal.GooseDB) error {
 	for i := 1; i <= 100; i++ {
 		q := "INSERT INTO foo VALUES ($1)"
 		if _, err := db.Exec(q, i); err != nil {

tests/gomigrations/success/testdata/005_up_down_no_tx.go
@@ -1,7 +1,7 @@
 package gomigrations
 
 import (
-	"database/sql"
+	"github.com/pressly/goose/v3/internal"
 
 	"github.com/pressly/goose/v3"
 )
@@ -10,13 +10,13 @@ func init() {
 	goose.AddMigrationNoTx(up005, down005)
 }
 
-func up005(db *sql.DB) error {
+func up005(db internal.GooseDB) error {
 	q := "CREATE TABLE users (id INT, email TEXT)"
 	_, err := db.Exec(q)
 	return err
 }
 
-func down005(db *sql.DB) error {
+func down005(db internal.GooseDB) error {
 	q := "DROP TABLE IF EXISTS users"
 	_, err := db.Exec(q)
 	return err

vegaprotocol/goosewithrunintxsupport

tests/e2e/allow_missing_test.go
@@ -1,7 +1,6 @@
 package e2e
 
 import (
-	"database/sql"
 	"testing"
 
 	"github.com/pressly/goose/v3"
@@ -317,8 +316,8 @@ func TestMigrateAllowMissingDown(t *testing.T) {
 
 // setupTestDB is helper to setup a DB and apply migrations
 // up to the specified version.
-func setupTestDB(t *testing.T, version int64) *sql.DB {
-	db, err := newDockerDB(t)
+func setupTestDB(t *testing.T, version int64) goose.Connection {
+	db, err := newDockerDB(t, true)
 	check.NoError(t, err)
 
 	// Create goose table.

tests/e2e/migrations_test.go
@@ -1,7 +1,7 @@
 package e2e
 
 import (
-	"database/sql"
+	"context"
 	"errors"
 	"fmt"
 	"path/filepath"
@@ -15,7 +15,7 @@ import (
 func TestMigrateUpWithReset(t *testing.T) {
 	t.Parallel()
 
-	db, err := newDockerDB(t)
+	db, err := newDockerDB(t, true)
 	check.NoError(t, err)
 	migrations, err := goose.CollectMigrations(migrationsDir, 0, goose.MaxVersion)
 	check.NoError(t, err)
@@ -43,7 +43,7 @@ func TestMigrateUpWithReset(t *testing.T) {
 func TestMigrateUpWithRedo(t *testing.T) {
 	t.Parallel()
 
-	db, err := newDockerDB(t)
+	db, err := newDockerDB(t, true)
 	check.NoError(t, err)
 	migrations, err := goose.CollectMigrations(migrationsDir, 0, goose.MaxVersion)
 	check.NoError(t, err)
@@ -80,7 +80,7 @@ func TestMigrateUpTo(t *testing.T) {
 	const (
 		upToVersion int64 = 2
 	)
-	db, err := newDockerDB(t)
+	db, err := newDockerDB(t, true)
 	check.NoError(t, err)
 	migrations, err := goose.CollectMigrations(migrationsDir, 0, goose.MaxVersion)
 	check.NoError(t, err)
@@ -101,7 +101,7 @@ func TestMigrateUpTo(t *testing.T) {
 func TestMigrateUpByOne(t *testing.T) {
 	t.Parallel()
 
-	db, err := newDockerDB(t)
+	db, err := newDockerDB(t, true)
 	check.NoError(t, err)
 	migrations, err := goose.CollectMigrations(migrationsDir, 0, goose.MaxVersion)
 	check.NoError(t, err)
@@ -131,7 +131,7 @@ func TestMigrateUpByOne(t *testing.T) {
 func TestMigrateFull(t *testing.T) {
 	t.Parallel()
 
-	db, err := newDockerDB(t)
+	db, err := newDockerDB(t, false)
 	check.NoError(t, err)
 	migrations, err := goose.CollectMigrations(migrationsDir, 0, goose.MaxVersion)
 	check.NoError(t, err)
@@ -208,9 +208,9 @@ func TestMigrateFull(t *testing.T) {
 	}
 }
 
-func getCurrentGooseVersion(db *sql.DB, gooseTable string) (int64, error) {
+func getCurrentGooseVersion(db goose.Connection, gooseTable string) (int64, error) {
 	var gotVersion int64
-	if err := db.QueryRow(
+	if err := db.QueryRowContext(context.Background(),
 		fmt.Sprintf("select max(version_id) from %s", gooseTable),
 	).Scan(&gotVersion); err != nil {
 		return 0, err
@@ -218,9 +218,9 @@ func getCurrentGooseVersion(db *sql.DB, gooseTable string) (int64, error) {
 	return gotVersion, nil
 }
 
-func getGooseVersionCount(db *sql.DB, gooseTable string) (int64, error) {
+func getGooseVersionCount(db goose.Connection, gooseTable string) (int64, error) {
 	var gotVersion int64
-	if err := db.QueryRow(
+	if err := db.QueryRowContext(context.Background(),
 		fmt.Sprintf("SELECT count(*) FROM %s WHERE version_id > 0", gooseTable),
 	).Scan(&gotVersion); err != nil {
 		return 0, err
@@ -228,7 +228,7 @@ func getGooseVersionCount(db *sql.DB, gooseTable string) (int64, error) {
 	return gotVersion, nil
 }
 
-func getTableNames(db *sql.DB) ([]string, error) {
+func getTableNames(db goose.Connection) ([]string, error) {
 	var query string
 	switch *dialect {
 	case dialectPostgres:
@@ -236,7 +236,7 @@ func getTableNames(db *sql.DB) ([]string, error) {
 	case dialectMySQL:
 		query = `SELECT table_name FROM INFORMATION_SCHEMA.tables WHERE TABLE_TYPE='BASE TABLE' ORDER BY table_name`
 	}
-	rows, err := db.Query(query)
+	rows, err := db.QueryContext(context.Background(), query)
 	if err != nil {
 		return nil, err
 	}

tests/gomigrations/error/testdata/002_ERROR_insert_no_tx.go
@@ -1,9 +1,7 @@
 package gomigrations
 
 import (
-	"database/sql"
 	"fmt"
-
 	"github.com/pressly/goose/v3"
 )
 
@@ -11,7 +9,7 @@ func init() {
 	goose.AddMigrationNoTx(up002, nil)
 }
 
-func up002(db *sql.DB) error {
+func up002(db goose.Connection) error {
 	for i := 1; i <= 100; i++ {
 		q := "INSERT INTO foo VALUES ($1)"
 		if _, err := db.Exec(q, i); err != nil {

tests/gomigrations/success/testdata/005_up_down_no_tx.go
@@ -1,8 +1,6 @@
 package gomigrations
 
 import (
-	"database/sql"
-
 	"github.com/pressly/goose/v3"
 )
 
@@ -10,13 +8,13 @@ func init() {
 	goose.AddMigrationNoTx(up005, down005)
 }
 
-func up005(db *sql.DB) error {
+func up005(db goose.Connection) error {
 	q := "CREATE TABLE users (id INT, email TEXT)"
 	_, err := db.Exec(q)
 	return err
 }
 
-func down005(db *sql.DB) error {
+func down005(db goose.Connection) error {
 	q := "DROP TABLE IF EXISTS users"
 	_, err := db.Exec(q)
 	return err